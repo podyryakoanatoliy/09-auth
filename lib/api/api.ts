f6fa6b5e14d1dc86b61f01cc9dbee66925dc18b6
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2025-12-30 14:25:01 +0200

    modified:   lib/api/api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "api",
   // baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
